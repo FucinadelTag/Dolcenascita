de1c1e8b7a3963675927e137f5820fbffb6aee9f
Lorenzo Caldara <lorenzo.caldara@gmail.com>
2018-07-27 14:14:34 +0200

    Aggiornamenti testi e immaigni

--- pages/index.vue
+++ pages/index.vue
@@ -16,52 +16,53 @@
                         <h2 class="uk-h3 uk-text-bold">Prima del PARTO</h2>
                         <img class="" :data-src="imageResize('/f/46475/1600x1027/8994c5caf0/prima-del-parto.jpg', '340x0')" alt="Doula" uk-img/>
 
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.</p>
+                        <p>La doula ti aiuta a diventare madre, percorrendo insieme i primi passi di questo meraviglioso cammino</p>
                         <dl class="uk-description-list">
                             <dt>Consigli pratici</dt>
-                            <dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit</dd>
+                            <dd>La doula sarà il tuo punto di riferimento e guida per il resto della gravidanza, saprà darti preziosi consigli per affrontare al meglio l’arrivo del tuo bimbo.</dd>
                         </dl>
                         <dl class="uk-description-list">
                             <dt>Ascolto e vicinanza</dt>
-                            <dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit</dd>
+                            <dd>La doula è una voce che ti ascolta e alla quale puoi confidare le tue preoccupazioni e paure, le tue gioie e aspettative.</dd>
                         </dl>
                         <dl class="uk-description-list">
                             <dt>Preparazione al parto</dt>
-                            <dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit</dd>
+                            <dd>Una doula ti preparerà al parto, permettendoti di affrontarlo in modo sereno e positivo. Insieme potrete praticare le tecniche di respirazione e le posizioni da assumere durante il travaglio.</dd>
                         </dl>
                     </div>
                     <div>
                         <h2 class="uk-h3 uk-text-bold">Durante il PARTO</h2>
                         <img class="" :data-src="imageResize('/f/46475/2121x1414/48e3e9d795/durante-in-parto.jpg', '340x0')" alt="Doula" uk-img/>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.</p>
+
+                        <p>La doula ti assiste durante il travaglio e il parto, fornendoti un continuo supporto emotivo e pratico.</p>
                         <dl class="uk-description-list">
                             <dt>Ti accompagna</dt>
-                            <dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit</dd>
+                            <dd>Avere una doula al tuo fianco ti farà affrontare questo grande giorno con più consapevolezza e serenità.</dd>
                         </dl>
                         <dl class="uk-description-list">
-                            <dt>Sta viciono al tuo compagno</dt>
-                            <dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit</dd>
+                            <dt>Sta viciono a te a al tuo compagno</dt>
+                            <dd>Il parto è un momento imprevedibile, ma la doula risponderà in ogni momento alle vostre domande per capire ciò che sta accadendo.</dd>
                         </dl>
                         <dl class="uk-description-list">
                             <dt>Ti da una mano</dt>
-                            <dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit</dd>
+                            <dd>La doula può darti suggerimenti sulle posizioni da assumere durante il travaglio, ti aiuta a concentrarti sulla respirazione durante le contrazioni più dolorose, allevia il dolore con massaggi mirati.</dd>
                         </dl>
                     </div>
                     <div>
                         <h2 class="uk-h3 uk-text-bold">Dopo il PARTO</h2>
                         <img class="" :data-src="imageResize('/f/46475/1688x1125/114ae32fce/dopo-il-parto1.jpg', '340x0')" alt="Doula" uk-img/>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.</p>
+                        <p>Dopo la nascita, la doula ti aiuterà ad avviare l’allattamento e a incoraggiare il legame tra il nuovo arrivato e il resto della famiglia.</p>
                         <dl class="uk-description-list">
                             <dt>Allattamento al seno</dt>
-                            <dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit</dd>
+                            <dd>L’allattamento al seno può a volte essere difficoltoso e molte madri rinunciano dopo poche settimane. La doula ti assisterà in ogni fase, assicurandoti che l’allattamento sia ben avviato, se lo vorrai.</dd>
                         </dl>
                         <dl class="uk-description-list">
                             <dt>Aiuto nei primi mesi</dt>
-                            <dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit</dd>
+                            <dd>La doula ti aiuterà a capire le necessità del nuovo nato, a riconoscere i suoi pianti e ad assisterlo, fornendoti un appoggio reale.</dd>
                         </dl>
                         <dl class="uk-description-list">
                             <dt>Ascolto e vicinanza</dt>
-                            <dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit</dd>
+                            <dd>I primi mesi sono i più difficili, soprattutto per le mamme. La doula ti fornirà un sostegno emotivo e fisico, sarà un aiuto su cui potrai contare.</dd>
                         </dl>
                     </div>
                 </div>
@@ -76,18 +77,18 @@
                 <hr>
                     <div class="uk-grid uk-grid-small uk-child-width-expand@s" uk-grid>
                         <div>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            <p class="uk-text-lead">
+                                La parola doula deriva dal greco e significa <br> <i>“servitrice delle donne”</i>.
+
                             </p>
-                            <p>
-                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            <p class="uk-text-lead">
+                                Una doula fa proprio questo: si prende cura della madre, sostenendola emotivamente e fisicamente dalla gravidanza fino ai primi mesi dopo la nascita. La doula aiuta le donne a vivere in modo sereno e memorabile uno dei momenti più delicati, impegnativi e straordinari della vita.
                             </p>
 
 
                         </div>
                         <div>
-                            <img class="" :data-src="imageResize('/f/46475/848x565/34b0af0548/prima-del-parto1.jpg', '600x0')" alt="Doula" uk-img />
+                            <img class="" :data-src="imageResize('/f/46475/848x565/34b0af0548/prima-del-parto1.jpg', '600x300')" alt="Doula" uk-img />
                         </div>
                     </div>
             </div>
@@ -173,9 +174,15 @@
             </div>
         </div>
 
-        <div class="uk-cover-container uk-height-max-medium">
-            <img :data-src="imageResize(immagine_testata, '1500x500')" alt="" uk-img>
+        <div class="uk-cover-container uk-height-medium">
+            <img :src="imageResize('/f/46475/3500x2333/050b7025a7/copertina.jpg', '2000x1000')" alt="" uk-cover>
         </div>
+
+        <!-- <div class="uk-section">
+            <img class="uk-height-large" :src="imageResize('/f/46475/3500x2333/050b7025a7/copertina.jpg', '2000x1000')" alt=""/>
+        </div> -->
+
+
         <div class="uk-section uk-section-muted uk-padding">
             <div class="uk-container">
                 <div class="uk-grid uk-grid-small uk-child-width-1-3@m uk-grid-divider" uk-grid>
--- tools/manageImages.js
+++ tools/manageImages.js
@@ -1,6 +1,7 @@
 const resize = function (image, option) {
   var imageService = '//img2.storyblok.com/'
   var path = image.replace('//a.storyblok.com', '')
+  console.log(path);
   return imageService  + option + '/smart' + path
 }
 
